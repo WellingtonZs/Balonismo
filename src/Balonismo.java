d1a21bb898746a9a02471d257e789311957db6ce
Wellington Cavalcante Ferreira de Lima <wellingtonzsescola@gmail.com>
2023-06-13 08:33:35 -0300

    .

--- src/Balonismo.java
+++ src/Balonismo.java
@@ -1,2 +1,12 @@
 public class Balonismo {
+
+    String passeio;
+
+    String nomepessoa;
+    int idade;
+    int cpf;
+    int rg;
+    int tel;
+    int telemergency;
+    int valor;
 }
--- src/Passeio.java
+++ src/Passeio.java
@@ -1,2 +1,13 @@
-public class Passeio {
+public class Passeio extends Balonismo {
+    String tipo;
+    String cor;
+    String substancias;
+    boolean fertil;
+
+    public Viagem gerarViagem(
+            Viagem s1
+    ){
+        Viagem p1 = new Viagem();
+        return p1;
+    }
 }
